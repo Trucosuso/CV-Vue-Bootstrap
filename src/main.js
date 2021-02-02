a8cc5b10c5fedc868a8c632df3ec0a4e480634ad
Jesús L. Pinzolas <jesus@trucosuso.com>
2021-02-03 00:13:11 +0100

    Added bootstrap to main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+// Import Bootstrap an BootstrapVue CSS files
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available
+Vue.use(BootstrapVue);
+// Install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+import App from "./App.vue";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
